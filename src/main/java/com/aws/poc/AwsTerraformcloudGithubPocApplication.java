5ac414fc76f03e005fa124a205ea807ee689340f
Ponraj <racingponraj@gmail.com>
2022-07-08 10:47:23 +0530

    AWS ec2 instance POC

---
+++ src/main/java/com/aws/poc/AwsTerraformcloudGithubPocApplication.java
@@ -0,0 +1,13 @@
+package com.aws.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsTerraformcloudGithubPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsTerraformcloudGithubPocApplication.class, args);
+	}
+
+}
